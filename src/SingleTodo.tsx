84ce2bbbbc572705d2eaeb6edc9fefb98288e12b
Bushrakhan15 <ummehabiba.bushra2000@gmail.com>
2025-07-02 22:21:49 +0600

    task management are done

--- src/SingleTodo.tsx
+++ src/SingleTodo.tsx
@@ -1,29 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Todo } from './modal';
 import './style.css';
 
-
-
-interface props{
-    todo: Todo,
-    todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const SingleTodo = ({todo,todos,setTodos}: props) => {
-  return <form  className='todos_single'>
-    <span className='todos_single_text'> {todo.todo} </span>
-      
-    <div>
-        <span className='icon'>Edit</span>
-        <span className='icon'>Delete</span>
-        <span className='icon'>Completed</span>
-    </div>
-  
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((t) =>
+        t.id === id ? { ...t, isDone: !t.isDone } : t
+      )
+    );
+  };
 
-  </form>
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((t) => t.id !== id));
+  };
 
-}
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((t) =>
+        t.id === id ? { ...t, todo: editTodo } : t
+      )
+    );
+    setEdit(false);
+  };
 
-export default SingleTodo
+  return (
+    <form
+      className="todos_single"
+      onSubmit={(e) => handleEdit(e, todo.id)}
+    >
+      {edit ? (
+        <input
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos_single_text"
+        />
+      ) : todo.isDone ? (
+        <s className="todos_single_text">{todo.todo}</s>
+      ) : (
+        <span className="todos_single_text">{todo.todo}</span>
+      )}
+
+      <div>
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(true);
+            }
+          }}
+        >
+          Edit
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          Delete
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          Completed
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
